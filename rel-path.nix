6124280fa40b272c66cfc86dee2e95f4e3bb3e31
Attila Gulyas <toraritte@gmail.com>
2023-04-05 23:21:33 -0400

    Print paths

--- rel-path.nix
+++ rel-path.nix
@@ -3,11 +3,14 @@ let
   puffin = ./a_file;
 in
   ''
+    ${pwd} --- ${puffin}
     Contents of "./puffin":
     ${builtins.readFile puffin}
 
     Listing the current working directory:
     ${builtins.concatStringsSep ", " (builtins.attrNames (builtins.readDir pwd))}
   ''
+
+  # "${pwd} --- ${puffin}"
 
 # vim: set foldmethod=marker foldmarker={{-,}}- foldlevelstart=0 tabstop=2 shiftwidth=2 expandtab:
