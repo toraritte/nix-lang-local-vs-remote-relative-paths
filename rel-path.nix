d3a0b5612aaccd094820869135d07aa6f109d840
Attila Gulyas <toraritte@gmail.com>
2023-04-05 23:39:51 -0400

    real simple

--- rel-path.nix
+++ rel-path.nix
@@ -1,11 +1,8 @@
 let
   pwd = ./.;
-  puffin = ./a_file;
 in
   ''
-    ${pwd} --- ${puffin}
+    ${pwd}
   ''
-
-  # "${pwd} --- ${puffin}"
 
 # vim: set foldmethod=marker foldmarker={{-,}}- foldlevelstart=0 tabstop=2 shiftwidth=2 expandtab:
