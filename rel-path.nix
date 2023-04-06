2521f32bf340445c7c3923caf44dd78527b14c22
Attila Gulyas <toraritte@gmail.com>
2023-04-05 23:15:28 -0400

    Add dir listing

--- rel-path.nix
+++ rel-path.nix
@@ -2,4 +2,12 @@ let
   pwd = ./.;
   puffin = ./a_file;
 in
-  builtins.readFile puffin
+  ''
+    Contents of "./puffin":
+    ${builtins.readFile puffin}
+
+    Listing the current working directory:
+    ${builtins.concatStringsSep ", " (builtins.attrNames (builtins.readDir pwd))}
+  ''
+
+# vim: set foldmethod=marker foldmarker={{-,}}- foldlevelstart=0 tabstop=2 shiftwidth=2 expandtab:
